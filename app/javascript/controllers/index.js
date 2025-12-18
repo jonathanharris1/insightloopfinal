34487a0935a73db479122fa8561cf7085a50ed2a
Mariana Fernandes Guarnieri <marianafernandesguarnieri@gmail.com>
2025-12-18 17:03:15 -0300

    heroku

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,5 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-import AiChatController from "./ai_chat_controller"
-application.register("ai-chat", AiChatController)
